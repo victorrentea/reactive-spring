febeb0a7772112dcd28830b062044ddb7c953842
Victor Rentea <victorrentea@gmail.com>
2021-03-08 23:02:40 +0200

    Reactor lite exercises

---
+++ src/test/java/victor/training/reactor/lite/Part04TransformTest.java
@@ -0,0 +1,59 @@
+package victor.training.reactor.lite;
+
+import victor.training.reactor.lite.domain.User;
+import victor.training.reactor.lite.repository.ReactiveRepository;
+import victor.training.reactor.lite.repository.ReactiveUserRepository;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+/**
+ * Learn how to transform values.
+ *
+ * @author Sebastien Deleuze
+ */
+public class Part04TransformTest {
+
+	Part04Transform workshop = new Part04Transform();
+	ReactiveRepository<User> repository = new ReactiveUserRepository();
+
+//========================================================================================
+
+	@Test
+	public void transformMono() {
+		Mono<User> mono = repository.findFirst();
+		StepVerifier.create(workshop.capitalizeOne(mono))
+				.expectNext(new User("SWHITE", "SKYLER", "WHITE"))
+				.verifyComplete();
+	}
+
+//========================================================================================
+
+	@Test
+	public void transformFlux() {
+		Flux<User> flux = repository.findAll();
+		StepVerifier.create(workshop.capitalizeMany(flux))
+				.expectNext(
+					new User("SWHITE", "SKYLER", "WHITE"),
+					new User("JPINKMAN", "JESSE", "PINKMAN"),
+					new User("WWHITE", "WALTER", "WHITE"),
+					new User("SGOODMAN", "SAUL", "GOODMAN"))
+				.verifyComplete();
+	}
+
+//========================================================================================
+
+	@Test
+	public void  asyncTransformFlux() {
+		Flux<User> flux = repository.findAll();
+		StepVerifier.create(workshop.asyncCapitalizeMany(flux))
+				.expectNext(
+					new User("SWHITE", "SKYLER", "WHITE"),
+					new User("JPINKMAN", "JESSE", "PINKMAN"),
+					new User("WWHITE", "WALTER", "WHITE"),
+					new User("SGOODMAN", "SAUL", "GOODMAN"))
+				.verifyComplete();
+	}
+
+}
