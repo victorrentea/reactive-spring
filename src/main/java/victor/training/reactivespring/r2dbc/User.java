3f0aa79536b5ceed17935914a242a447a156f40f
Victor Rentea <victorrentea@gmail.com>
2021-03-09 16:23:17 +0200

    R2dbc sample

---
+++ src/main/java/victor/training/reactivespring/r2dbc/User.java
@@ -0,0 +1,18 @@
+package victor.training.reactivespring.r2dbc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("users")
+@Data
+public class User {
+   @Id
+   private Integer id;
+   private String name;
+
+   public User(String name) {
+      this.name = name;
+   }
+}
