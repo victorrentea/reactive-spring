57b87fab78490660d5647177233f13af254dd184
Victor Rentea <victorrentea@gmail.com>
2021-03-10 00:47:37 +0200

    Thread hopping

--- src/main/java/victor/training/reactor/hotcold/HotCold.java
+++ src/main/java/victor/training/reactor/hotcold/HotCold.java
@@ -1,24 +1,43 @@
 package victor.training.reactor.hotcold;
 
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.ConnectableFlux;
-import reactor.core.publisher.Flux;
+import org.assertj.core.api.Assertions;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
-import victor.training.reactivespring.start.ThreadUtils;
+import reactor.test.StepVerifier;
 
-import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
+import java.util.function.Function;
 
 @Slf4j
 public class HotCold {
-   public static void main(String[] args) {
 
-      Mono.just("Hello")
-          .flatMap(s-> Mono.deferContextual(
-              context -> Mono.just(s + " " + context.get("username"))))
-          .contextWrite(context -> context.put("username","John"));
+   public static String captureThread(String workType) {
+       return workType + ":" + Thread.currentThread().getName();
+   }
+   public static void main(String[] args) {
+      List<String> steps =new ArrayList<>();
+
+      Runnable readTask = () -> steps.add(captureThread("READ"));
+
+      Runnable cpuTask = () -> steps.add(captureThread("CPU"));
+      Runnable writeTask = () -> steps.add(captureThread("WRITE"));
+
+
+      Mono<String> mono = Mono.just("a")
+          .then(Mono.fromRunnable(readTask))
+          .subscribeOn(Schedulers.boundedElastic())
+          .publishOn(Schedulers.parallel())
+          .then(Mono.fromRunnable(cpuTask))
+          .publishOn(Schedulers.boundedElastic())
+          .then(Mono.fromRunnable(writeTask))
+          ;
+      StepVerifier.create(mono)
+          .verifyComplete();
+      Assertions.assertThat(steps.toString())
+          .contains("READ:boundedElastic")
+          .contains("CPU:parallel")
+          .contains("WRITE:boundedElastic");
    }
 }
--- src/test/java/victor/training/reactor/lite/Part12AdvancedTest.java
+++ src/test/java/victor/training/reactor/lite/Part12AdvancedTest.java
@@ -1,5 +1,6 @@
 package victor.training.reactor.lite;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -7,6 +8,7 @@ import reactor.test.StepVerifier;
 import victor.training.reactivespring.start.ThreadUtils;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -55,4 +57,46 @@ public class Part12AdvancedTest {
           .verifyComplete();
       assertThat(duration).isGreaterThan(Duration.ofMillis(900));
    }
+
+   private String captureThread(String workType) {
+      return workType + ":" + Thread.currentThread().getName();
+   }
+   @Test
+   public void threadHopping() {
+      List<String> steps =new ArrayList<>();
+
+      Runnable readTask = () -> steps.add(captureThread("READ"));
+      Runnable cpuTask = () -> steps.add(captureThread("CPU"));
+      Runnable writeTask = () -> steps.add(captureThread("WRITE"));
+
+      Mono<Void> mono = workshop.threadHopping(readTask, cpuTask, writeTask);
+
+      StepVerifier.create(mono).verifyComplete();
+
+      Assertions.assertThat(steps.toString())
+          .contains("READ:boundedElastic")
+          .contains("CPU:parallel")
+          .contains("WRITE:boundedElastic");
+   }
+
+   @Test
+   public void threadHoppingHard() {
+      List<String> steps =new ArrayList<>();
+
+      Runnable readTask = () -> steps.add(captureThread("READ"));
+      Runnable cpuTask = () -> steps.add(captureThread("CPU"));
+      Runnable writeTask = () -> steps.add(captureThread("WRITE"));
+
+      Mono<Void> sourceMono = Mono.fromRunnable(readTask);
+      Mono<Void> mono = workshop.threadHoppingHard(sourceMono, cpuTask);
+      Mono<Void> finalMono = mono.then(Mono.fromRunnable(writeTask));
+
+      StepVerifier.create(finalMono).verifyComplete();
+
+      Assertions.assertThat(steps.toString())
+          .contains("READ:boundedElastic")
+          .contains("CPU:parallel")
+          .contains("WRITE:boundedElastic");
+   }
+
 }
