1b12c6e9876f9b77fdc8911bbd70944c2afa9eb3
Victor Rentea <victorrentea@gmail.com>
2021-03-09 11:30:41 +0200

    Inlantuire de op pe th pooluri separate - ex cu CF - done

--- src/main/java/victor/training/reactivespring/start/MixingExecutors.java
+++ src/main/java/victor/training/reactivespring/start/MixingExecutors.java
@@ -2,14 +2,25 @@ package victor.training.reactivespring.start;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.*;
+
 @Slf4j
 public class MixingExecutors {
 
    public static void main(String[] args) {
 
-      int i = ioRead();
-      int i2 = cpu(i);
-      ioWrite(i2);
+//      int i = ioRead();
+//      int i2 = cpu(i);
+//      ioWrite(i2);
+
+      ExecutorService ioPool = Executors.newFixedThreadPool(1);
+      ExecutorService cpuPool = Executors.newFixedThreadPool(1);
+
+      CompletableFuture.supplyAsync(MixingExecutors::ioRead, ioPool)
+          .thenApplyAsync(MixingExecutors::cpu, cpuPool)
+          .thenAcceptAsync(MixingExecutors::ioWrite, ioPool);
+
+      ThreadUtils.sleep(400); // threadurile din ForkJoinPool.commonPool sunt daemon. App se inchide daca au ramas doar astea in viata
    }
 
    public static int ioRead() {
@@ -23,5 +34,7 @@ public class MixingExecutors {
    }
    public static void ioWrite(int i) {
       log.info("write " + i );
+      ThreadUtils.sleep(100);
+      log.info("Done");
    }
 }
