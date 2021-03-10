df44c977b42fc11ece79846be3d3dbe4c8989399
Victor Rentea <victorrentea@gmail.com>
2021-03-10 10:48:11 +0200

    COmplex flow #1: non-blocking REST calls

--- src/main/java/victor/training/reactor/complex/ComplexFlow.java
+++ src/main/java/victor/training/reactor/complex/ComplexFlow.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -12,7 +13,6 @@ import victor.training.reactivespring.start.ThreadUtils;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Supplier;
 
 @Slf4j
 public class ComplexFlow {
@@ -31,7 +31,7 @@ public class ComplexFlow {
           .subscribe(list -> log.info("Result: " + list))
       ;
       log.info("Gata");
-      ThreadUtils.sleep(4000);
+      ThreadUtils.sleep(30000);
    }
 
    private static Mono<List<Product>> mainFlow(List<Long> productIds) {
@@ -43,7 +43,7 @@ public class ComplexFlow {
    }
    public static Mono<Product> convertBlockingToReactive(Long productId) {
       return Mono
-          .defer(() ->Mono.just(ExternalAPI.getProductDetails(productId)))
+          .defer(() -> ExternalAPI.getProductDetails(productId))
           .subscribeOn(Schedulers.boundedElastic())
           ;
    }
@@ -52,14 +52,19 @@ public class ComplexFlow {
 @Slf4j
 class ExternalAPI {
    @SneakyThrows
-   public static Product getProductDetails(Long productId) {
+   public static Mono<Product> getProductDetails(Long productId) {
       // ne inchipuim apel REST / WS
       log.info("Calling REST");
 
-      RestTemplate rest = new RestTemplate();
-      ProductDto dto = rest.getForObject("http://localhost:9999/api/product/1", ProductDto.class);
-
-      return new Product(dto.getName(), dto.isActive());
+//      RestTemplate rest = new RestTemplate();
+//      ProductDto dto = rest.getForObject("http://localhost:9999/api/product/1", ProductDto.class);
+//      return Mono.just(new Product(dto.getName(), dto.isActive()));
+//
+      return WebClient.create().get().uri("http://localhost:9999/api/product/1")
+          .retrieve()
+          .bodyToMono(ProductDto.class)
+         .map(dto -> new Product(dto.getName(), dto.isActive()))
+      ;
    }
 }
 
