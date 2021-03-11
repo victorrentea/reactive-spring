cb9f3864f53261ba1491d043ad2bcb4626dcb98e
Victor Rentea <victorrentea@gmail.com>
2021-03-11 10:20:28 +0200

    Multe moduri

--- src/main/java/victor/training/reactor/lite/Part11BlockingToReactive.java
+++ src/main/java/victor/training/reactor/lite/Part11BlockingToReactive.java
@@ -42,17 +42,28 @@ public class Part11BlockingToReactive {
 	//  using an elastic scheduler and return a Mono<Void> that signal the end of the operation
 	Mono<Void> fluxToBlockingRepository(Flux<User> flux, BlockingRepository<User> repository) {
 
+		return flux
+			.publishOn(Schedulers.boundedElastic())
+			.doOnNext(repository::save)
+//			.flatMap(user -> Mono.fromRunnable( () -> {
+//				repository.save(user);
+//			})
+//		)
+			.log()
+			.then()
+			;
 
-		Mono<Void> mono = Mono.<Void>fromRunnable(() -> {
-			for (User user : flux.toIterable()) {
-				repository.save(user);
-			}
-			// COD
 
-		}).subscribeOn(Schedulers.boundedElastic());
+//		Mono<Void> mono = Mono.<Void>fromRunnable(() -> {
+//			for (User user : flux.toIterable()) {
+//				repository.save(user);
+//			}
+//			// COD
+//
+//		}).subscribeOn(Schedulers.boundedElastic());
 
 
-		return mono;
+//		return mono;
 	}
 
 }
