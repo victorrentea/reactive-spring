09242975a8355723e9f340cd683c7beb077679b8
Victor Rentea <victorrentea@gmail.com>
2021-03-16 22:51:03 +0200

    Threads undo

--- src/main/java/victor/training/reactor/lite/Part13Threads.java
+++ src/main/java/victor/training/reactor/lite/Part13Threads.java
@@ -13,35 +13,21 @@ public class Part13Threads {
 
    // TODO Run readTask on the Schedulers#boundedElastic, and return a mono of the returned value in UPPERCASE
    public Mono<String> subscribe(Supplier<String> readTask) {
-      return Mono.fromSupplier(readTask)
-          .subscribeOn(Schedulers.boundedElastic());
+      return null;
    }
 
    //========================================================================================
 
    // TODO Run readTask on the Schedulers#boundedElastic followed by the cpuTask on the Schedulers#parallel
    public Mono<Void> ioVsCpu(Runnable ioTask, Runnable cpuTask) {
-      return Mono.fromRunnable(ioTask)
-          .subscribeOn(Schedulers.boundedElastic())
-          .publishOn(Schedulers.parallel())
-          .then(Mono.fromRunnable(cpuTask));
+      return null;
    }
 
    //========================================================================================
 
    // TODO Run readTask on the Schedulers#boundedElastic in parallel with the cpuTask ran on the Schedulers#parallel
    public Mono<Void> ioVsCpuParallel(Runnable ioTask, Runnable cpuTask) {
-
-      Mono<Void> ioMono = Mono.fromRunnable(ioTask)
-          .subscribeOn(Schedulers.boundedElastic())
-          .then();
-
-      Mono<Void> cpuMono = Mono.fromRunnable(cpuTask)
-          .subscribeOn(Schedulers.parallel())
-          .then();
-
-
-      return Mono.when(ioMono, cpuMono);
+      return null;
    }
 
 
@@ -55,12 +41,7 @@ public class Part13Threads {
 
    // TODO Run readTask and writeTask on the Schedulers#boundedElastic and cpuTask on the Schedulers#parallel
    public Mono<?> threadHopping(RxService service) {
-      return service.readData()
-          .subscribeOn(Schedulers.boundedElastic())
-          .publishOn(Schedulers.parallel())
-          .flatMap(s -> service.cpuTask(s))
-          .publishOn(Schedulers.boundedElastic())
-          .flatMap(i -> service.writeData(i));
+      return null;
    }
 
    //========================================================================================
@@ -72,12 +53,7 @@ public class Part13Threads {
 
    // TODO Same as above, but with non-reactive APIs
    public Mono<?> threadHoppingNonMonoApi(BlockingService service) {
-      return Mono.fromSupplier(() -> service.readData())
-          .subscribeOn(Schedulers.boundedElastic())
-          .publishOn(Schedulers.parallel())
-          .flatMap(s -> Mono.defer(() -> Mono.just(service.cpuTask(s))))
-          .publishOn(Schedulers.boundedElastic())
-          .flatMap(i -> Mono.fromRunnable(() -> service.writeData(i)));
+      return null;
    }
 
 
@@ -86,10 +62,7 @@ public class Part13Threads {
    // TODO the same as above, but the read and write happen in the caller (before and after you are invoked) - see the test
    public Mono<Integer> threadHoppingHard(Mono<String> sourceMono, Function<String, Mono<Integer>> cpuTask) {
       return sourceMono
-          .subscribeOn(Schedulers.boundedElastic())
-          .publishOn(Schedulers.parallel())
           .flatMap(s -> cpuTask.apply(s))
-          .publishOn(Schedulers.boundedElastic())
           ;
    }
 }
