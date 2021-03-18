fcd768d85bd4cb8ae741e2c484e8d9cc892324e7
Victor Rentea <victorrentea@gmail.com>
2021-03-18 08:57:15 +0200

    undo grouping

--- src/main/java/victor/training/reactor/dynamic/Grouping.java
+++ src/main/java/victor/training/reactor/dynamic/Grouping.java
@@ -11,11 +11,6 @@ import java.util.List;
 public class Grouping {
    public static void main(String[] args) {
       // TODO odd numbers should be sent in pages of 10 items to sendOdd, while even numbers to sendEven, respectively
-      Flux.range(0,100)
-      .groupBy(i -> i % 2 == 0 ? ItemType.EVEN: ItemType.ODD)
-      .flatMap(groupedFlux -> groupedFlux.buffer(10).map(list -> new ItemPageWithType(groupedFlux.key(), list)))
-      .flatMap(x -> x.getItemType() == ItemType.EVEN ? sendEven(x.getPage()):sendOdd(x.getPage()))
-      .subscribe();
 
 
    }
@@ -25,15 +20,4 @@ public class Grouping {
    public static Mono<Void> sendOdd(List<Integer> page) {
       return Mono.fromRunnable(() -> log.info("Sending odd: {}", page));
    }
-}
-
-@Value
-class ItemPageWithType {
-   ItemType itemType;
-   List<Integer> page;
-}
-enum ItemType {
-
-   EVEN,
-   ODD
 }
