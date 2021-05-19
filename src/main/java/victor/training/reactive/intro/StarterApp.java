e911d921a05cdb9afea5289b4f8ec84bc0470fd9
Victor Rentea <victorrentea@gmail.com>
2021-05-19 11:42:36 +0300

    CF end

--- src/main/java/victor/training/reactive/intro/StarterApp.java
+++ src/main/java/victor/training/reactive/intro/StarterApp.java
@@ -3,6 +3,7 @@ package victor.training.reactive.intro;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,10 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
-import static java.util.concurrent.CompletableFuture.completedFuture;
+import java.util.concurrent.*;
 
 @Slf4j
 @EnableAsync
@@ -48,6 +46,7 @@ public class StarterApp  {
       executor.setCorePoolSize(corePoolSize);
       executor.setMaxPoolSize(corePoolSize);
       executor.setQueueCapacity(500);
+//      executor.setRejectedExecutionHandler(new CallerRunsPolicy());
       executor.setThreadNamePrefix("vodka-");
       executor.initialize();
       executor.setWaitForTasksToCompleteOnShutdown(true);
@@ -56,52 +55,88 @@ public class StarterApp  {
 
    private final Barman barman;
 
-//      static ExecutorService pool = Executors.newFixedThreadPool(4);
+   @Autowired
+   ThreadPoolTaskExecutor beerPool;
+   @Autowired
+   ThreadPoolTaskExecutor vodkaPool;
 
 
    @GetMapping("dilly")
    public CompletableFuture<DillyDilly> run() throws ExecutionException, InterruptedException {
+
       log.info("Sending orders calls to the barman : " + barman.getClass());
       long t0 = System.currentTimeMillis();
 
-      // TODO async servlets
       // TODO CF plain Java
       // TODO controlling pool sizes
       // TODO Exceptions
 
-      CompletableFuture<Beer> futureBeer = barman.pourBeer();
-      CompletableFuture<Vodka> futureVodka = barman.pourVodka();
+      CompletableFuture<Beer> futureBeer = CompletableFuture
+          .supplyAsync(() -> barman.pourBlondBeer(), beerPool)
+          .exceptionally(e -> {
+             if (e.getCause() instanceof IllegalStateException) {
+                return barman.pourDarkBeer();
+             }
+             throw new RuntimeException(e);
+          });
+      CompletableFuture<Vodka> futureVodka = CompletableFuture.supplyAsync(() -> barman.pourVodka(), vodkaPool);
 
       log.debug("Now, two workers are pouring drinks for me in ||");
 
+      barman.curse("&!^@&#^@!&$^!*@$**%(!");
+
 
       CompletableFuture<DillyDilly> futureDilly = futureBeer.thenCombineAsync(futureVodka, DillyDilly::new);
-
       long t1 = System.currentTimeMillis();
       log.debug("Time= " + (t1 - t0));
       return futureDilly;
-
    }
+
+   // later in spring
+//   {
+//      CompletableFuture<DillyDilly> dillyCF;
+//
+//      dillyCF.thenAccept(dilly -> {
+//
+//      })
+//   }
 }
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 class Barman {
-   @Async("beerPool")
-   public CompletableFuture<Beer> pourBeer() {
+//   @Async("beerPool")
+   public Beer pourBlondBeer() {
       log.info("Start pour beer");
+      if (true) {
+         throw new IllegalStateException("Out of beer");
+      }
       ThreadUtils.sleep(1000); // blocking REST call
       log.info("end pour beer");
-      return completedFuture(new Beer());
+      return new Beer();
+   }
+   public Beer pourDarkBeer() {
+      log.info("Start pour dark beer");
+      ThreadUtils.sleep(1000); // blocking REST call
+      log.info("end pour beer");
+      return new Beer();
    }
 
-   @Async("vodkaPool")
-   public CompletableFuture<Vodka> pourVodka() {
+//   @Async("vodkaPool")
+   public Vodka pourVodka() {
       log.info("Start pour vodka");
       ThreadUtils.sleep(1000);  // blocking DB call
       log.info("end pour vodka");
-      return completedFuture(new Vodka());
+      return new Vodka();
+   }
+
+   @Async
+   public void curse(String curse) {
+      if (curse != null) {
+         throw new RuntimeException("Bring the boys");
+      }
+
    }
 }
 
