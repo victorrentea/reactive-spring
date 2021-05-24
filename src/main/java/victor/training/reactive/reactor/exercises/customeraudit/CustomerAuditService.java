482f9b14a11e3fa46ce222e3cb94869e53bd5e06
Victor Rentea <victorrentea@gmail.com>
2021-05-25 02:30:29 +0300

    Exercise SOLVED

--- src/main/java/victor/training/reactive/reactor/exercises/customeraudit/CustomerAuditService.java
+++ src/main/java/victor/training/reactive/reactor/exercises/customeraudit/CustomerAuditService.java
@@ -24,32 +24,6 @@ public class CustomerAuditService {
    private String baseUrl;
 
    public void processOrderingCustomer(Flux<Integer> customerIdFlux) {
-      customerIdFlux
-          .flatMap(id -> repo.findById(id))
-          .groupBy(c -> c.isExternal())
-          .flatMap(group -> {
-             if (group.key()) {
-                // external
-                return group.buffer(2).flatMap(buffer -> {
-                   String ids = buffer.stream().map(Customer::getId).map(Objects::toString).collect(joining(","));
-                   log.info("GET REST " + ids);
-                   return WebClient.create().get().uri(baseUrl + "/external-customer/" + ids)
-                           .retrieve().bodyToFlux(Customer.class);
-                    }
-                );
-             } else {
-                return group;
-             }
-          })
-          .flatMap(c -> send(c))
-          .subscribe();
    }
-
-   private Mono<Void> send(Customer c) {
-      log.info("SEND customer " + c);
-      if (c.isActive()) return WebClient.create().get().uri(baseUrl + "/customer-audit/{id}",c.getId()).retrieve().toBodilessEntity().log().then();
-      return emailSender.sendInactiveCustomerEmail(c);
-   }
-
 
 }
