d30e59040f91e7573768316ab9c8b94290e14687
Victor Rentea <victorrentea@gmail.com>
2021-06-21 15:42:12 +0300

    Fetch one by one over network. 100 in paralel -- > DOS attack

--- src/main/java/victor/training/reactive/reactor/complex/ComplexFlow.java
+++ src/main/java/victor/training/reactive/reactor/complex/ComplexFlow.java
@@ -2,11 +2,13 @@ package victor.training.reactive.reactor.complex;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
@@ -15,31 +17,28 @@ public class ComplexFlow {
 
    public static void main(String[] args) {
       List<Long> productIds = LongStream.range(1, 10).boxed().collect(Collectors.toList());
-
-      Mono<List<Product>> listMono = mainFlow(productIds);
+//      mainFlow(productIds).flatMap(cassandraRepo::save);
+      Mono<List<Product>> listMono = mainFlow(productIds).collectList();
 
       List<Product> products = listMono.block(); // unusual, only here to stop main thread from exiting
       log.info("Done. Got {} products: {}", products.size(), products);
    }
 
 
-   private static Mono<List<Product>> mainFlow(List<Long> productIds) {
-      List<Product> result = new ArrayList<>();
-      for (Long productId : productIds) {
-         Product product = getProductById(productId);
-         result.add(product);
-      }
-
-      return Mono.just(result);
+   private static Flux<Product> mainFlow(List<Long> productIds) {
+     return Flux.fromIterable(productIds)
+          .flatMap(ComplexFlow::getProductById, 100);
    }
 
    @SneakyThrows
-   public static Product getProductById(Long productId) {
+   public static Mono<Product> getProductById(Long productId) {
       log.info("Calling Get Product Details REST");
-      RestTemplate rest = new RestTemplate();
-      ProductDetailsResponse dto = rest.getForObject("http://localhost:9999/api/product/{id}", ProductDetailsResponse.class, productId);
-      return dto.toEntity();
-      // TODO 1 use a non-blockin REST client
+      return WebClient.create()
+          .get()
+          .uri("http://localhost:9999/api/product/{id}", productId)
+          .retrieve()
+          .bodyToMono(ProductDetailsResponse.class)
+          .map(ProductDetailsResponse::toEntity);
       // TODO 2 is it ok to call them N times over network ?
       //....
    }
