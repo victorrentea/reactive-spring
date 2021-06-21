011b610f1786c45baf0be42e862c41b9bdd68419
Victor Rentea <victorrentea@gmail.com>
2021-06-21 15:51:34 +0300

    get the data in pages from their api, unmarshall into List<Dto>

--- src/main/java/victor/training/reactive/reactor/complex/ComplexFlow.java
+++ src/main/java/victor/training/reactive/reactor/complex/ComplexFlow.java
@@ -2,6 +2,7 @@ package victor.training.reactive.reactor.complex;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -12,11 +13,13 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
+import static java.util.stream.Collectors.toList;
+
 @Slf4j
 public class ComplexFlow {
 
    public static void main(String[] args) {
-      List<Long> productIds = LongStream.range(1, 10).boxed().collect(Collectors.toList());
+      List<Long> productIds = LongStream.range(1, 10).boxed().collect(toList());
 //      mainFlow(productIds).flatMap(cassandraRepo::save);
       Mono<List<Product>> listMono = mainFlow(productIds).collectList();
 
@@ -26,19 +29,22 @@ public class ComplexFlow {
 
 
    private static Flux<Product> mainFlow(List<Long> productIds) {
-     return Flux.fromIterable(productIds)
-          .flatMap(ComplexFlow::getProductById, 100);
+      return Flux.fromIterable(productIds) // FLux<id>
+          .buffer(2)// Flux<List<id>>   = Flux<page of ids>
+          .flatMap(ComplexFlow::getProductById, 2)
+          .flatMap(list -> Flux.fromIterable(list));
    }
 
    @SneakyThrows
-   public static Mono<Product> getProductById(Long productId) {
+   public static Mono<List<Product>> getProductById(List<Long> productIds) {
       log.info("Calling Get Product Details REST");
-      return WebClient.create()
-          .get()
-          .uri("http://localhost:9999/api/product/{id}", productId)
+      return (Mono<List<Product>>) WebClient.create()
+          .post()
+          .uri("http://localhost:9999/api/product/many")
+          .body(Mono.just(productIds.toString()), String.class)
           .retrieve()
-          .bodyToMono(ProductDetailsResponse.class)
-          .map(ProductDetailsResponse::toEntity);
+          .bodyToMono(new ParameterizedTypeReference<List<ProductDetailsResponse>>() {})
+          .map(listDto -> listDto.stream().map(ProductDetailsResponse::toEntity).collect(toList()));
       // TODO 2 is it ok to call them N times over network ?
       //....
    }
