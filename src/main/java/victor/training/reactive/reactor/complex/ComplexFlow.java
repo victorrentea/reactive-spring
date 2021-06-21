4edd032b8896f2f7a903e9e42dfc2d29b7b7ae73
Victor Rentea <victorrentea@gmail.com>
2021-06-21 16:53:10 +0300

    Complex problem - audit.subscribe()

--- src/main/java/victor/training/reactive/reactor/complex/ComplexFlow.java
+++ src/main/java/victor/training/reactive/reactor/complex/ComplexFlow.java
@@ -1,5 +1,6 @@
 package victor.training.reactive.reactor.complex;
 
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
@@ -10,6 +11,7 @@ import reactor.core.publisher.Mono;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
@@ -23,30 +25,56 @@ public class ComplexFlow {
 //      mainFlow(productIds).flatMap(cassandraRepo::save);
       Mono<List<Product>> listMono = mainFlow(productIds).collectList();
 
-      List<Product> products = listMono.block(); // unusual, only here to stop main thread from exiting
-      log.info("Done. Got {} products: {}", products.size(), products);
+
+//      CompletableFuture<String> cf = CompletableFuture.supplyAsync(() -> "a");
+//      Mono<String> stringMono = Mono.fromCompletionStage(cf);
+
+      listMono.subscribe(products -> {
+//         List<Product> products = listMono.block(); // unusual, only here to stop main thread from exiting
+         log.info("Done. Got {} products: {}", products.size(), products);
+      });
+
    }
 
 
    private static Flux<Product> mainFlow(List<Long> productIds) {
-      return Flux.fromIterable(productIds) // FLux<id>
+      return Flux.fromIterable(productIds)
           .buffer(2)// Flux<List<id>>   = Flux<page of ids>
-          .flatMap(ComplexFlow::getProductById, 2)
-          .flatMap(list -> Flux.fromIterable(list));
+          // the set if ids will be broken in pages of 2 items (eg 10 items => [1,2], [3,4], ... [9,10]
+          .flatMap(ComplexFlow::getProductById, 3) // only 3 pages will be sent in parallel to their API. eg [1,2] || [3,4] || [5,6]
+
+          .doOnNext(p -> auditResealedProduct(p).subscribe())
+
+          ;
+//      Stream.peak()
    }
 
    @SneakyThrows
-   public static Mono<List<Product>> getProductById(List<Long> productIds) {
+   public static Mono<Void> auditResealedProduct(Product product) {
+      // TODO only audit resealed products !
+      return WebClient.create().get().uri("http://localhost:9999/api/audit-resealed/" + product)
+          .retrieve()
+          .toBodilessEntity()
+          .doOnSubscribe(s -> log.info("Calling Audit REST"))
+          .then();
+   }
+
+   @SneakyThrows
+   public static Flux<Product> getProductById(List<Long> productIds) {
+
       log.info("Calling Get Product Details REST");
-      return (Mono<List<Product>>) WebClient.create()
+      String hugeString = productIds.toString(); // 10 MB
+      return WebClient.create()
           .post()
           .uri("http://localhost:9999/api/product/many")
-          .body(Mono.just(productIds.toString()), String.class)
+
+          .body(Flux.fromIterable(productIds), Long.class) // GOOD streaming data as it's toStringed
+//          .body(Mono.just(hugeString), String.class) // BAD for huge request
+
           .retrieve()
-          .bodyToMono(new ParameterizedTypeReference<List<ProductDetailsResponse>>() {})
-          .map(listDto -> listDto.stream().map(ProductDetailsResponse::toEntity).collect(toList()));
-      // TODO 2 is it ok to call them N times over network ?
-      //....
+          .bodyToFlux(ProductDetailsResponse.class)
+          .map(ProductDetailsResponse::toEntity)
+          ;
    }
 
 }
