d8ee8372cd9185f04b6fb4c7ce1748a557fc4d22
Victor Rentea <victorrentea@gmail.com>
2021-06-21 11:23:58 +0300

    Thread pools with spring

--- src/main/java/victor/training/reactive/intro/StarterApp.java
+++ src/main/java/victor/training/reactive/intro/StarterApp.java
@@ -3,6 +3,8 @@ package victor.training.reactive.intro;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,14 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
-import reactor.core.scheduler.Schedulers;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Vector;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 @Slf4j
@@ -31,11 +26,11 @@ public class StarterApp implements CommandLineRunner {
    }
 
    @Bean
-   public ThreadPoolTaskExecutor pool() {
+   public ThreadPoolTaskExecutor pool( @Value("${barman.count}")int barmanCount) {
       ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-      executor.setCorePoolSize(2);
-      executor.setMaxPoolSize(2);
-      executor.setQueueCapacity(500);
+      executor.setCorePoolSize(barmanCount);
+      executor.setMaxPoolSize(barmanCount);
+      executor.setQueueCapacity(2);
       executor.setThreadNamePrefix("bar-");
       executor.initialize();
       executor.setWaitForTasksToCompleteOnShutdown(true);
@@ -44,7 +39,8 @@ public class StarterApp implements CommandLineRunner {
 
    private final Barman barman;
 
-      ExecutorService pool = Executors.newFixedThreadPool(12);
+   @Autowired
+   ThreadPoolTaskExecutor pool;
 
    @Override
    public void run(String... args) throws Exception {
