6385d4256d00bd9a17e6cacb038f6ef0252604ba
Victor Rentea <victorrentea@gmail.com>
2021-06-23 02:45:41 +0300

    compilation

--- src/main/java/victor/training/reactive/reactor/game/SillyRxScheduler.java
+++ src/main/java/victor/training/reactive/reactor/game/SillyRxScheduler.java
@@ -1,33 +1,33 @@
-//package victor.training.reactive.reactor.game;
-//
-//import javafx.application.Platform;
-//import reactor.core.Disposable;
-//import reactor.core.scheduler.Scheduler;
-//import reactor.util.annotation.NonNull;
-//
-//public class SillyRxScheduler implements Scheduler {
-//   @Override
-//   @NonNull
-//   public Disposable schedule(@NonNull Runnable task) {
-//      Platform.runLater(task);
-//      return () -> { };
-//   }
-//
-//   @Override
-//   @NonNull
-//   public Worker createWorker() {
-//      return new Worker() {
-//         @Override
-//         @NonNull
-//         public Disposable schedule(@NonNull Runnable task) {
-//            Platform.runLater(task);
-//            return () -> { };
-//         }
-//
-//         @Override
-//         public void dispose() {
-//
-//         }
-//      };
-//   }
-//}
+package victor.training.reactive.reactor.game;
+
+import javafx.application.Platform;
+import reactor.core.Disposable;
+import reactor.core.scheduler.Scheduler;
+import reactor.util.annotation.NonNull;
+
+public class SillyRxScheduler implements Scheduler {
+   @Override
+   @NonNull
+   public Disposable schedule(@NonNull Runnable task) {
+      Platform.runLater(task);
+      return () -> { };
+   }
+
+   @Override
+   @NonNull
+   public Worker createWorker() {
+      return new Worker() {
+         @Override
+         @NonNull
+         public Disposable schedule(@NonNull Runnable task) {
+            Platform.runLater(task);
+            return () -> { };
+         }
+
+         @Override
+         public void dispose() {
+
+         }
+      };
+   }
+}
