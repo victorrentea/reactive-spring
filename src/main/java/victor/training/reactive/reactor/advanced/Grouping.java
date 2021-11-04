cc2dacd821668f218732cb7a1e315e9c11c481c1
Victor Rentea <victorrentea@gmail.com>
2021-11-04 15:45:38 +0200

    Grouped Flux + orchestare prin enum cu bifunctii.

--- src/main/java/victor/training/reactive/reactor/advanced/Grouping.java
+++ src/main/java/victor/training/reactive/reactor/advanced/Grouping.java
@@ -3,49 +3,106 @@ package victor.training.reactive.reactor.advanced;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 import victor.training.reactive.intro.ThreadUtils;
 
 import java.util.List;
+import java.util.function.BiFunction;
 
 import static reactor.core.scheduler.Schedulers.boundedElastic;
 
 @Slf4j
 public class Grouping {
    public static void main(String[] args) {
+      new Grouping().run();
+   }
+
+   public void run() {
+
       // TODO send odd numbers to sendOdd(), while even numbers to sendEven(), respectively
       // TODO send even numbers in pages of 10 items
       // TODO for odd numbers call preSendOdd() before sendOdd()
       // TODO item 0 should not trigger any sending
 
       Flux.range(0, 100)
-          .flatMap(Grouping::sendOdd)
+
+          .groupBy(NumberType::on)
+//          .flatMap(groupedFlux -> {
+//             if (groupedFlux.key() == NumberType.EVEN) {
+//               return processEven(groupedFlux);
+//             } else {
+//               return processOdd(groupedFlux);
+//             }
+//          })
+
+          // antipattern pe java 17
+          .flatMap(groupedFlux -> groupedFlux.key().processFunction.apply(this, groupedFlux))
+
+          // java 17:
+//          .flatMap(groupedFlux -> switch (groupedFlux.key()) {
+//                 case EVEN -> processEven(groupedFlux);
+//                 case ODD -> processOdd(groupedFlux);
+          // nu compileaza daca adaugi valori noi la enum
+//              }
+//          )
+
+
           .subscribe();
+
 
       ThreadUtils.sleep(2000);
    }
 
-   public static NumberType getType(int number) {
+   public Mono<Void> processEven(Flux<Integer> groupedFlux) {
+      return groupedFlux.buffer(10).flatMap(page -> sendEven(page)).then();
+   }
+
+   public Mono<Void> processOdd(Flux<Integer> groupedFlux) {
+      return groupedFlux.buffer(10)
+          .flatMap(page -> sendOdd(page).mergeWith(sendOdd2(page))).then();
+   }
+
+
+   public NumberType getType(int number) {
       return number % 2 == 1 ? NumberType.ODD : NumberType.EVEN;
    }
 
-   public static Mono<Void> sendEven(Integer item) {
+   public Mono<Void> sendEven(List<Integer> item) {
       return Mono.<Void>fromRunnable(() -> log.info("Sending even numbers: {}", item))
-          .subscribeOn(boundedElastic())
-          ;
+          .subscribeOn(boundedElastic());
    }
 
-   public static Mono<Void> sendOdd(Integer item) {
+   public Mono<Void> sendOdd(List<Integer> item) {
+      // de ce trebuie de instanta sa fie? ca vrea acces la @Autowired rx repos injectate
       return Mono.<Void>fromRunnable(() -> log.info("Sending odd numbers: {}", item))
           .subscribeOn(boundedElastic())
           ;
    }
 
-   public static Mono<Void> preSendOdd(Integer item) {
+   public Mono<Void> sendOdd2(List<Integer> item) {
+      return Mono.<Void>fromRunnable(() -> log.info("Sending odd numbers: {}", item))
+          .subscribeOn(boundedElastic())
+          ;
+   }
+
+   public Mono<Void> preSendOdd(Integer item) {
       return Mono.fromRunnable(() -> System.out.println("Pre send odd " + item));
+   }
+
+   enum NumberType {
+      ODD(Grouping::processOdd),
+      EVEN(Grouping::processEven)
+      //NEGATIVE
+      ;
+
+      public final BiFunction<Grouping, Flux<Integer>, Mono<Void>> processFunction;
+
+      NumberType(BiFunction<Grouping, Flux<Integer>, Mono<Void>> process) {
+         this.processFunction = process;
+      }
+
+      public static NumberType on(Integer n) {
+         return n % 2 == 1 ? NumberType.ODD : NumberType.EVEN;
+      }
    }
 }
 
-enum NumberType {
-   ODD, EVEN
-}
