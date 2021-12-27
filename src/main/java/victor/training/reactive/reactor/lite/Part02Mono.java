56f1ce9e40f10889feb31e071a30614242079aa6
Eduardo <e.gonblan@gmail.com>
2021-12-27 13:19:44 +0100

    Part02 - Mono

--- src/main/java/victor/training/reactive/reactor/lite/Part02Mono.java
+++ src/main/java/victor/training/reactive/reactor/lite/Part02Mono.java
@@ -6,37 +6,37 @@ public class Part02Mono {
 
 //========================================================================================
 
-	// TODO Return an empty Mono
+	// Return an empty Mono
 	public Mono<String> emptyMono() {
-		return null;
+		return Mono.empty();
 	}
 
 //========================================================================================
 
-	// TODO Return a Mono that never emits any signal
+	// Return a Mono that never emits any signal
 	public Mono<String> monoWithNoSignal() {
-		return null;
+		return Mono.never();
 	}
 
 //========================================================================================
 
-	// TODO Return a Mono that contains a "foo" value
+	// Return a Mono that contains a "foo" value
 	public Mono<String> fooMono() {
-		return null;
+		return Mono.just("foo");
 	}
 
 //========================================================================================
 
-	// TODO Return a Mono of data. data can come null.
+	// Return a Mono of data. data can come null.
 	public Mono<String> optionalMono(String data) {
-		return null;
+		return Mono.justOrEmpty(data);
 	}
 
 //========================================================================================
 
-	// TODO Create a Mono that emits an IllegalStateException
+	// Create a Mono that emits an IllegalStateException
 	public Mono<String> errorMono() {
-		return null;
+		return Mono.error(IllegalStateException::new);
 	}
 
 }
