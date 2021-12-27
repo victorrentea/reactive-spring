c8a8f649e5ee0f43523c27ceed97d16274520eb7
Eduardo <e.gonblan@gmail.com>
2021-12-27 18:01:18 +0100

    Part04 - Transforms

--- src/main/java/victor/training/reactive/reactor/lite/Part04Transform.java
+++ src/main/java/victor/training/reactive/reactor/lite/Part04Transform.java
@@ -8,23 +8,29 @@ public class Part04Transform {
 
 //========================================================================================
 
-	// TODO Capitalize the user username, firstname and lastname
+	// Capitalize the user username, firstname and lastname
 	public Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map(user -> new User(
+				user.getUsername().toUpperCase(),
+				user.getFirstname().toUpperCase(),
+				user.getLastname().toUpperCase()));
 	}
 
 //========================================================================================
 
-	// TODO Capitalize the users username, firstName and lastName
+	// Capitalize the users username, firstName and lastName
 	public Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(user -> new User(
+				user.getUsername().toUpperCase(),
+				user.getFirstname().toUpperCase(),
+				user.getLastname().toUpperCase()));
 	}
 
 //========================================================================================
 
-	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
+	// Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
 	public Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.flatMap(this::asyncCapitalizeUser);
 	}
 
 	protected Mono<User> asyncCapitalizeUser(User u) {
