087fedd7242662f8f2bc76f041fb3213b4094f33
Victor Rentea <victorrentea@gmail.com>
2022-01-21 01:00:24 +0200

    fix complilation

--- src/main/java/victor/training/reactive/reactor/lite/TheTaskPlay.java
+++ src/main/java/victor/training/reactive/reactor/lite/TheTaskPlay.java
@@ -1,26 +1,26 @@
-package victor.training.reactive.reactor.lite;
-
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import reactor.core.publisher.Sinks;
-import reactor.core.publisher.Sinks.Many;
-
-public class TheTaskPlay {
-
-   public static void main(String[] args) {
-
-
-      Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
-
-
-
-      sink.asFlux().subscribe(s -> System.out.println("1: " + s));
-      sink.tryEmitNext("a");
-      sink.asFlux()
-          .doOnTerminate()
-          .subscribe(s -> System.out.println("1: " + s));
-      Mono<String> m1,m2;
-
-//      Flux.zip(m1,m2, (s1,s2) -> 1);
-   }
-}
+//package victor.training.reactive.reactor.lite;
+//
+//import reactor.core.publisher.Flux;
+//import reactor.core.publisher.Mono;
+//import reactor.core.publisher.Sinks;
+//import reactor.core.publisher.Sinks.Many;
+//
+//public class TheTaskPlay {
+//
+//   public static void main(String[] args) {
+//
+//
+//      Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
+//
+//
+//
+//      sink.asFlux().subscribe(s -> System.out.println("1: " + s));
+//      sink.tryEmitNext("a");
+//      sink.asFlux()
+//          .doOnTerminate()
+//          .subscribe(s -> System.out.println("1: " + s));
+//      Mono<String> m1,m2;
+//
+////      Flux.zip(m1,m2, (s1,s2) -> 1);
+//   }
+//}
